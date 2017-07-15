4c14967747986ab38ed9618ccf8d8372d8be89ca
vikrant <info.vikrant.verma@gmail.com>
2017-07-15 10:47:56 +0530

    code

--- src/com/vikrant/linkedlist/LinkedList1.java
+++ src/com/vikrant/linkedlist/LinkedList1.java
@@ -6,6 +6,8 @@ public class LinkedList1 {
 		LinkedList a = new LinkedList();
 		a.addFromRight(10);
 		a.addFromRight(20);
+		a.addFromLeft(5);
+		a.addAfterValue(11, 10);
 		System.out.println(a);		
 	}
 
@@ -31,35 +33,68 @@ class LinkedList
 	Node next;
 	
 	
-	
-	
 	void addFromRight(int value) {
 		if (head != null) {
 			next = new Node();
 			next.data = value;
 			head.address = next;
-			//System.out.println("next :"+next);
-
 		} else {
-			head = new Node();
-			head.data = value;
-			next = head;
-			//System.out.println("head :"+head);
+			addFirstNode(value);
 		}
 	}
 
-	
+	void addFirstNode(int value){
+		head = new Node();
+		head.data = value;
+		next = head;
+	}
 	
 	
 	void addFromLeft(int value){
+		if(head!=null){
+			Node temp=head;
+			head= new Node();
+			head.data=value;
+			head.address=temp;
+		}else{
+			addFirstNode(value);
+
+		}
+		
 		
 	}
+	
+	
+	void addAfterValue(int value,int afterValue){
+		if(head!=null){
+			Node h1=head;
+			while(h1!=null){
+				if(h1.data==afterValue){
+					Node temp=h1.address;
+					Node newNode=new Node();
+					newNode.data=value;
+					newNode.address=temp;
+					h1.address=newNode;
+					break;
+					
+				}
+				
+				h1=h1.address;
+			}
+			
+		}else{
+			addFirstNode(value);
+
+		}
+	}
+	
+	
+	
 	
 	String iterateList() {
 		Node h1 = head;
 		String ret = "[";
 		while (h1 != null) {
-			//System.out.println(h1.data);
 			ret = ret + h1.data + ",";
 			h1 = h1.address;
 		}
